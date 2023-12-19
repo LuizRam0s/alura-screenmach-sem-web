019c105e369cc0db0724086a3e05a8df5ebca8c7
Ramos <luizramos128@hotmail.com>
2023-12-19 06:02:32 -0300

    Subindo projeto SreenMach sem web

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias ("title") String titulo,
+                         @JsonAlias ("totalSeasons") Integer  totaltemporadas,
+                         @JsonAlias ("imdbRating") String avaliacao) {
+
+}
